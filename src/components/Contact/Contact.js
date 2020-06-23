59663dddc5b588f7afa8d6fff07f78f72d6743a0
BrianDeSplinter <bdesplinter34@gmail.com>
2020-06-23 13:52:08 -0600

    Added

---
+++ src/components/Contact/Contact.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react'
+
+class Contact extends Component {
+    constructor() {
+        super()
+        this.state = {
+
+        }
+    }
+    render() {
+        return(
+            <div>I am the Contact Page!</div>
+        )
+    }
+}
+
+export default Contact
