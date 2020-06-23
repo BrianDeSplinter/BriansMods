af6aaccdf44a75adfce50126c64e02cdbc473246
BrianDeSplinter <bdesplinter34@gmail.com>
2020-06-23 15:27:07 -0600

    Formatting

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -9,7 +9,16 @@ class About extends Component {
     }
     render() {
         return(
-            <div>I am the About Page!</div>
+            <div>
+                <h3>
+                    I am the About Page!
+                </h3>
+                <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. In rhoncus velit eu ornare condimentum. Donec dignissim iaculis nibh et imperdiet. Quisque lobortis vulputate elementum. Aenean dapibus leo et vehicula mattis. Nullam erat arcu, tincidunt quis placerat quis, tempor vitae libero. Fusce in egestas ipsum, vel euismod nisl. Proin pulvinar ante eu est elementum porttitor. In ullamcorper magna a condimentum iaculis. Maecenas eu bibendum magna. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Nunc ante dui, ornare vitae blandit vel, dictum eu diam.
+                <br/>
+                Morbi mattis nec ipsum quis malesuada. Vivamus non faucibus libero. Suspendisse vulputate lectus libero, sit amet facilisis enim venenatis et. Morbi purus erat, viverra quis elit sit amet, maximus imperdiet ex. Etiam faucibus gravida nisi et vulputate. Nam eros quam, mollis id lobortis facilisis, molestie vel enim. Quisque ultrices sem in justo porta, eu sodales erat egestas. Integer efficitur semper magna, non aliquam purus lacinia ac. Aliquam erat volutpat. Ut in semper purus, vitae sollicitudin lacus. Donec nec turpis finibus, mollis erat molestie, placerat nibh. Curabitur venenatis id dui at faucibus. Morbi in augue condimentum, egestas dolor eu, lobortis erat. Mauris tincidunt, nisl ac consectetur cursus, orci odio placerat nunc, quis dignissim ipsum sapien sed lorem. Duis euismod feugiat rhoncus. Quisque rutrum laoreet neque et interdum.
+                </p>    
+            </div>
         )
     }
 }
