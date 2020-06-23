bb99187b7bdd8b1dffa13193da21f81fba18800a
BrianDeSplinter <bdesplinter34@gmail.com>
2020-06-23 15:26:30 -0600

    Completed DB seed

---
+++ db/seed.sql
@@ -0,0 +1,77 @@
+CREATE TYPE "products_status" AS ENUM (
+  'out_of_stock',
+  'running_low',
+  'in_stock'
+);
+
+CREATE TYPE "order_status" AS ENUM (
+  'pending',
+  'fulfilled',
+  'shipped'
+);
+
+CREATE TYPE "product_type" AS ENUM (
+  'performance',
+  'engine',
+  'suspension',
+  'exhaust',
+  'interior',
+  'exterior',
+  'merch'
+);
+
+CREATE TABLE "users" (
+  "id" SERIAL PRIMARY KEY,
+  "full_name" varchar(100),
+  "email" varchar(200),
+  "password" text,
+  "created_at" timestamp
+);
+
+CREATE TABLE "admins" (
+  "id" SERIAL PRIMARY KEY,
+  "admin_name" text,
+  "admin_id" int
+);
+
+CREATE TABLE "products" (
+  "id" SERIAL PRIMARY KEY,
+  "name" text,
+  "imgage_url" text,
+  "price" decimal,
+  "status" products_status,
+  "merchant_id" int NOT NULL,
+  "created_at" timestamp,
+  "catergory" product_type,
+  "description" text,
+  "notes" text
+);
+
+CREATE TABLE "order_items" (
+  "order_id" int,
+  "product_id" int,
+  "quantity" int DEFAULT 1,
+  "total_price" decimal
+);
+
+CREATE TABLE "orders" (
+  "id" SERIAL PRIMARY KEY,
+  "user_id" int NOT NULL,
+  "total" decimal,
+  "created_at" timestamp,
+  "status" order_status
+);
+
+CREATE TABLE "merchants" (
+  "id" SERIAL PRIMARY KEY,
+  "merchant_name" text
+);
+
+
+ALTER TABLE "admins" ADD FOREIGN KEY ("admin_id") REFERENCES "users" ("id");
+
+ALTER TABLE "products" ADD FOREIGN KEY ("merchant_id") REFERENCES "merchants" ("id");
+
+ALTER TABLE "order_items" ADD FOREIGN KEY ("order_id") REFERENCES "orders" ("id");
+
+ALTER TABLE "order_items" ADD FOREIGN KEY ("product_id") REFERENCES "products" ("id");
