94312fdaddda4425771cbd03131048e6f3cbe006
BrianDeSplinter <bdesplinter34@gmail.com>
2020-06-25 10:50:01 -0600

    Setup

---
+++ server/controllers/inventoryController.js
@@ -0,0 +1,21 @@
+module.exports = {
+    getInventory:
+        async (req, res) => {
+
+        },
+    
+    addProduct:
+        async (req, res) => {
+
+        },
+
+    updateProduct:
+        async (req, res) => {
+
+        },
+
+    deleteProduct:
+        async (req, res) => {
+            
+        }
+}
