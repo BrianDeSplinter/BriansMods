436ace15038e6109b93ed827aa6b8b5033096dd0
BrianDeSplinter <bdesplinter34@gmail.com>
2020-06-25 12:33:15 -0600

    Added logout functionality

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,18 +1,27 @@
 import React, {Component} from 'react'
 import {Link} from 'react-router-dom'
+import {connect} from 'react-redux'
+import {logoutUser} from '../../redux/loginReducer'
 import './Navbar.css'
+import axios from 'axios'
 
 class Navbar extends Component {
-    constructor() {
-        super()
-        this.state = {
-
-        }
+    
+    logoutHandler = (e) => {
+        axios.delete('/auth/logout')
+        .then(res => {
+            this.props.logoutUser()
+            //this.props.history.push('/')
+        })
     }
-    render() {
+
+    render(props) {
+        const {isLoggedIn} = this.props
+       
         return(
             <div>
-                <h3>I am the Navbar!</h3>
+                <h2>Brian's Mods</h2>
+                <h3>{isLoggedIn ? 'logged in' : 'logged out'}</h3>
                 <div>
                     <nav>
                         <Link to='/'>Home</Link>
@@ -21,7 +30,7 @@ class Navbar extends Component {
                         <br/>
                         <Link to='/contact'>Contact Us</Link>
                         <br/>
-                        <Link to='/login'>Login</Link>
+                        {isLoggedIn ? <button onClick={e => this.logoutHandler(e)}>Logout</button> : <Link to='/login'>Login</Link>}
                         <br/>
                         <Link to='/cart'>Cart</Link>
                     </nav>
@@ -31,4 +40,45 @@ class Navbar extends Component {
     }
 }
 
-export default Navbar
+const mapStateToProps = reduxState => reduxState
+
+export default connect(mapStateToProps, {logoutUser})(Navbar)
+
+
+
+
+// --------------------
+// import React, {Component} from 'react'
+// import {Link} from 'react-router-dom'
+// import {connect} from 'react-redux'
+// import './Navbar.css'
+
+// class Navbar extends Component {
+    
+//     render(props) {
+//         const {isLoggedIn} = this.props
+//         return(
+//             <div>
+//                 <h2>Brian's Mods</h2>
+//                 <h3>{isLoggedIn ? 'logged in' : 'logged out'}</h3>
+//                 <div>
+//                     <nav>
+//                         <Link to='/'>Home</Link>
+//                         <br/>
+//                         <Link to='/about'>About</Link>
+//                         <br/>
+//                         <Link to='/contact'>Contact Us</Link>
+//                         <br/>
+//                         {isLoggedIn ? <Link to='/logout'>Logout</Link> : <Link to='/login'>Login</Link>}
+//                         <br/>
+//                         <Link to='/cart'>Cart</Link>
+//                     </nav>
+//                 </div>
+//             </div>
+//         )
+//     }
+// }
+
+// const mapStateToProps = reduxState => reduxState
+
+// export default connect(mapStateToProps)(Navbar)
