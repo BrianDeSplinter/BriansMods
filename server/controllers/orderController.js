6acfb2b3e4938c1f96570fb34734a8e3a01280c9
BrianDeSplinter <bdesplinter34@gmail.com>
2020-06-25 17:06:31 -0600

    Completed addOrder method

--- server/controllers/orderController.js
+++ server/controllers/orderController.js
@@ -1,11 +1,22 @@
 module.exports = {
     addItems: 
         async (req, res) => {
+            const db = req.app.get('db')
 
+            const {items} = req.body
+            //const {orderId, productId, quantity, total} = req.body
+            
         },
     
     addOrder:
         async (req, res) => {
-            
+            const db = req.app.get('db')
+
+            const {user, total} = req.body
+            const status = 'pending'
+
+            const id = await db.add_order(user, total, status)
+
+            res.status(200).send(id)
         }
 }
