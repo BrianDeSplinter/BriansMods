325afafc84600333f90e8d44204c2b80b576912f
BrianDeSplinter <bdesplinter34@gmail.com>
2020-06-25 17:02:24 -0600

    Added timestamp in update method

--- db/update_product.sql
+++ db/update_product.sql
@@ -1,3 +1,3 @@
 UPDATE products 
-SET name = $1, image_url = $2, price = $3, status = $4, merchant_id = $5, category = $6, description = $7, notes = $8
-WHERE id = $9;
+SET name = $1, image_url = $2, price = $3, status = $4, merchant_id = $5, last_edit = $6, category = $7, description = $8, notes = $9
+WHERE id = $10;
