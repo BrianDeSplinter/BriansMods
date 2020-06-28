e152a7f4da8c598df69e66ed534c6b8e66e5a50a
BrianDeSplinter <bdesplinter34@gmail.com>
2020-06-27 19:02:46 -0600

    Added functionality

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,17 +1,76 @@
 import React, {Component} from 'react'
+import axios from 'axios'
+import {connect} from 'react-redux'
+import {addToCart} from '../../redux/cartReducer'
 
 class Product extends Component {
     constructor() {
         super()
         this.state = {
-
+            //loading: true
+            cart: [],
+            quantity: 1
         }
     }
+
+    componentDidMount(){
+        axios.get(`/product/${this.props.match.params.id}`)
+        .then(res => {
+            this.setState({
+                ...res.data, 
+                // loading:false
+            })
+        })
+    }
+
+    componentWillUnmount(){
+        console.log('Unmounted!')
+    }
+
+    increaseQuantity(){
+        this.setState({
+            quantity: this.state.quantity + 1
+        })
+    }
+
+    decreaseQuantity(){
+        if(this.state.quantity > 1){
+            this.setState({
+                quantity: this.state.quantity - 1
+            })
+        }
+    }
+
+    // addToCart(){
+
+    // }
+
     render() {
+        console.log(this.state.quantity)
         return(
-            <div>I am the individual product page!</div>
+            <div>
+                <h2>I am the individual product page!</h2>
+                <div className='product'>
+                    <h4>{this.state.name}</h4>
+                    <img src={this.state.image_url} alt='failed to load :('/>
+                    <h5>Price: ${this.state.price}</h5>
+                    <h5>Category: {this.state.category}</h5>
+                    <p>Description: {this.state.description}</p>
+                    <p>Notes: {this.state.notes}</p>
+                    <h5>Availability: {this.state.status}</h5>
+                    <button
+                        onClick={(e) => this.decreaseQuantity(e)}
+                    >-</button>
+                    <p>{this.state.quantity}</p>
+                    <button
+                        onClick={(e) => this.increaseQuantity(e)}   
+                    >+</button>
+                    <button>Add to Cart</button>
+                </div>
+            </div>
         )
     }
 }
 
-export default Product
+
+export default connect(null, {addToCart})(Product)
