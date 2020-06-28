4c99370dc6cccab0597bdf59e983e470eab4b070
BrianDeSplinter <bdesplinter34@gmail.com>
2020-06-27 19:04:05 -0600

    Added funtionality to view products

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,15 +1,42 @@
 import React, {Component} from 'react'
+import axios from 'axios'
+import Products from '../Products/Products'
 
 class Home extends Component {
     constructor() {
         super()
         this.state = {
-
+            inventory: []
         }
     }
+
+    componentDidMount(){
+        axios.get('/products').then(res =>{
+            this.setState({
+                inventory: res.data
+            })
+        })
+    }
+
     render() {
+        const products = this.state.inventory.map(e => (
+            <Products
+                key={e.id}
+                id={e.id}
+                name={e.name}
+                image={e.image_url}
+                price={e.price}
+                status={e.status}
+                category={e.category}
+                description={e.description}
+                notes={e.notes}
+            />
+        ))
         return(
-            <div>I am Home!</div>
+            <div>
+                <h3>I am Home!</h3>
+                {products}
+            </div>
         )
     }
 }
