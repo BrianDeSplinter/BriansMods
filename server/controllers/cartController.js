54de0aace3c92118b670b8a1c98dc23859a5b967
BrianDeSplinter <bdesplinter34@gmail.com>
2020-06-30 00:23:03 -0600

    Test Code

--- server/controllers/cartController.js
+++ server/controllers/cartController.js
@@ -1,11 +1,19 @@
 module.exports = {
     getCart:
-        async (req, res) => {
-
+        (req, res) => {
+            // if(req.session.user) {
+            //     res.status(200).send(req.session.cart)
+            // } else {
+            //     res.sendStatus(200)
+            // }
         },
     
     addCart:
-        async (req, res) => {
-
+        (req, res) => {
+            // const {cart} = req.body
+            // req.session.user = {
+            //     cart: cart
+            // }
+            // res.send(200).send(req.session.user)
     }
 }
