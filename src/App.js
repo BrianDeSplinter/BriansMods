d59424b34e5b879b06c843b82ac91424a314f608
BrianDeSplinter <bdesplinter34@gmail.com>
2020-06-30 16:44:45 -0600

    Moved getUser to nabar

--- src/App.js
+++ src/App.js
@@ -1,18 +1,31 @@
 import React, {Component} from 'react';
 import Navbar from './components/Navbar/Navbar'
 import routes from './routes'
-import {connect} from 'react-redux'
-import {getUser} from './redux/loginReducer'
+// import axios from 'axios'
+// import {connect} from 'react-redux'
+// import {getUser} from './redux/loginReducer'
+// import {getCart} from './redux/cartReducer'
+// import {updateCart} from './redux/cartReducer'
 import './App.css';
 
 class App extends Component{
   
-  componentDidMount() {
-    this.props.getUser()
-  }
+  // componentDidMount() {
+  //   this.props.getUser()
+  // }
+
+  // componentDidMount() {
+  //   this.props.getCart()
+  // }
+
+  // componentWillUnmount() {
+  //   axios.post('/cart', [this.props.cart])
+  //   .then(res => console.log('Props sent to sessions'))
+  //   this.props.updateCart()
+  // }
 
   render() {
-    // console.log(this.props)
+    //console.log('app.js', this.props.cart)
     return (
       <div className="App">
         <Navbar/>
@@ -22,5 +35,6 @@ class App extends Component{
   }
 }
 
-const mapStateToProps = reduxState => reduxState
-export default connect(mapStateToProps, {getUser})(App);
+//const mapStateToProps = reduxState => reduxState
+export default App
+// connect(mapStateToProps, {getUser, getCart, updateCart})(App);
