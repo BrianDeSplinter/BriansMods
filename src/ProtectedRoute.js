2cec1e58ddf5cfb0899e64f60b06a8d38a83d3c6
BrianDeSplinter <bdesplinter34@gmail.com>
2020-06-30 23:15:30 -0600

    Formatting

--- src/ProtectedRoute.js
+++ src/ProtectedRoute.js
@@ -4,18 +4,17 @@ import { useSelector } from 'react-redux'
 
 
 const ProtectedRoute = (props) => {
-  const {path, component} = props
+    const {path, component} = props
     
 
-  const isLoggedIn = useSelector(
-    (reduxState) => reduxState.login.isLoggedIn
-  )
+    const isLoggedIn = useSelector(
+        (reduxState) => reduxState.login.isLoggedIn
+    )
 
-  const isAdmin = useSelector(
-    (reduxState) => reduxState.login.user.adminId
-  )
+    const isAdmin = useSelector(
+        (reduxState) => reduxState.login.user.adminId
+    )
   
-//   return isLoggedIn ? <Route path={path} component={component}/> : <Redirect to='/' />
     if(isLoggedIn && isAdmin){
         return <Route path={path} component={component}/>
     } else {
